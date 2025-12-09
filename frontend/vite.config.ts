777dda450cae1f2f3e982cca4ee849f24531e075
Oh Zhan Jie Alexander <watterson332@gmail.com>
2025-12-10 01:39:15 +0800

    remove https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from "fs";
+// import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,10 +10,10 @@ export default defineConfig({
     fs: {
       strict: false, // allow serving files from public
     },
-    https: {
-      key: fs.readFileSync("./cert/key.pem"),
-      cert: fs.readFileSync("./cert/cert.pem")
-    }
+    // https: {
+    //   key: fs.readFileSync("./cert/key.pem"),
+    //   cert: fs.readFileSync("./cert/cert.pem")
+    // }
   },
   build: {
     rollupOptions: {
